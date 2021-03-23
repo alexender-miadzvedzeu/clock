cfe734b0dcc7a12d5c3e8d3335e487e54fffda45
Alexender <medved3993@gmail.com>
2021-03-23 18:26:12 +0300

    all done!

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-let clock = document.querySelector('.clock')
-let secondArrow = document.getElementById('secondArrow')
-console.log(clock, secondArrow);
+let secondsArrow = document.getElementById('secondsArrow');
+let minutesArrow = document.getElementById('minutesArrow');
+let hourArrow = document.getElementById('hourArrow');
+setInterval(() => {
+    let date = new Date;
+    let currentSecond = date.getSeconds();
+    let currentMinute = date.getMinutes();
+    let currentHour;
+    date.getHours() > 12 ? currentHour = (date.getHours() - 12) : currentHour = (date.getHours());
+    secondsArrow.style.transform = `rotate(${currentSecond * 6}deg)`;
+    minutesArrow.style.transform = `rotate(${currentMinute * 6}deg)`;
+    hourArrow.style.transform = `rotate(${currentHour * 30}deg)`;
+}, 1000);
